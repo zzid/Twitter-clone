cf914db33950e84327a823fc20066580a31192a8
zzid <931010dy@gmail.com>
2020-10-23 17:05:24 +0900

    Updates

---
+++ precache-manifest.9a59a1a015b6957d148a154149c87eeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64649d41748c0e44ad3ce7cb31b5eda3",
+    "url": "/Twitter-clone/index.html"
+  },
+  {
+    "revision": "4e6fbdb15d1f38a72ef3",
+    "url": "/Twitter-clone/static/css/main.d62f1eca.chunk.css"
+  },
+  {
+    "revision": "841504b3db83793b6fc6",
+    "url": "/Twitter-clone/static/js/2.0b646a14.chunk.js"
+  },
+  {
+    "revision": "8e844923efaee6906e00c8fde9e55716",
+    "url": "/Twitter-clone/static/js/2.0b646a14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e6fbdb15d1f38a72ef3",
+    "url": "/Twitter-clone/static/js/main.6fa416d8.chunk.js"
+  },
+  {
+    "revision": "13ca68e59c15f8fa3eb7",
+    "url": "/Twitter-clone/static/js/runtime-main.92ffe739.js"
+  }
+]);
